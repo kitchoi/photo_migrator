d07890a676cddd7da553777a936c4c5eea30a60f
Kit Choi <kit@kychoi.org>
2017-07-02 17:01:02 +0100

    Add stream argument for set_logger

--- photo_migrator/utils/log_utils.py
+++ photo_migrator/utils/log_utils.py
@@ -1,16 +1,29 @@
 import contextlib
 import logging
+import sys
 
 
 _FORMAT = "%(levelname)s - %(funcName)s(%(lineno)d) %(message)s"
 
 
 @contextlib.contextmanager
-def set_logger(logger, level):
+def set_logger(logger, level, stream=sys.stdout):
+    """ Context manager for setting a logger to a given logging level and
+    an output stream.
+
+    Parameters
+    ----------
+    logger : logging.Logger
+        Logger to be set.
+    level : int
+        Logging level.
+    stream : file-like object
+        Output stream for the logger.
+    """
     old_level = logger.level
     old_handlers = list(logger.handlers)
 
-    new_handler = logging.StreamHandler()
+    new_handler = logging.StreamHandler(stream=stream)
     new_handler.setLevel(level)
     new_handler.setFormatter(logging.Formatter(fmt=_FORMAT))
 
