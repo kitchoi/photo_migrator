434a156c6db53aacd451e66c36e3e8ea68c059de
Kit Choi <kit@kychoi.org>
2017-07-02 18:14:51 +0100

    Fix import after name change

--- photo_migrator/tests/test_cli.py
+++ photo_migrator/tests/test_cli.py
@@ -9,13 +9,13 @@ from photo_migrator import cli
 from photo_migrator.utils import testing
 
 
-class TestPhotom(test_utils.TempDirMixin, unittest.TestCase):
+class TestPhotom(testing.TempDirMixin, unittest.TestCase):
     """ Test photom commands """
 
     def setUp(self):
-        test_utils.TempDirMixin.setUp(self)
+        testing.TempDirMixin.setUp(self)
         self.source_dir = os.path.join(self.temp_dir, "source_dir")
-        shutil.copytree(test_utils.TEST_IMAGES_DIR, self.source_dir)
+        shutil.copytree(testing.TEST_IMAGES_DIR, self.source_dir)
         self.out_dir = os.path.join(self.temp_dir, "out_dir")
         os.mkdir(self.out_dir)
 
@@ -49,7 +49,7 @@ class TestPhotom(test_utils.TempDirMixin, unittest.TestCase):
         self.check_cli_result(result)
 
         # Check the larger image is downsized
-        large_filename = os.path.basename(test_utils.IMG_NO_DATETIME_LARGE)
+        large_filename = os.path.basename(testing.IMG_NO_DATETIME_LARGE)
         in_file = os.path.join(self.source_dir, large_filename)
         in_size = os.path.getsize(in_file)
         out_file = os.path.join(self.out_dir, large_filename)
