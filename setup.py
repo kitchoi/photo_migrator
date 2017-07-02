9d6913b438952acebef6e9855ffc4456a4b3c8d6
Kit Choi <kit@kychoi.org>
2017-07-02 14:50:08 +0100

    Update requirement

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     version='0.1',
     py_modules=['photo_migrator'],
     install_requires=[
+        'attrs',
         'click',
+        'pillow',
     ],
     entry_points={
         "console_scripts": [
